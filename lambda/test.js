f6020049291f680f3a5eba54f1c447e3ec0e6249
Martin Burolla <mburolla@xpanxion.com>
2022-04-05 14:02:06 -0400

    update

--- lambda/test.js
+++ lambda/test.js
@@ -2,13 +2,13 @@ const AWS = require('aws-sdk')
 
 const S3 = new AWS.S3({region: 'us-east-1'})
 
-const main = async () => {
+const upload = async () => {
     let b = { firstName: "Joe", lastName: "Smith" };
     const buffer = Buffer.from(JSON.stringify(b), "utf-8");
     await S3.upload({ Bucket: 'sia-test-2', Key: 'joe.txt', Body: buffer }).promise()
 }
 
-main();
+upload();
 
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
