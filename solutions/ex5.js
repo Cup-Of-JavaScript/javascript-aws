9d8b014a85ef3f6dd1e0ac69a24e26ccafcadf6e
Martin Burolla <mburolla@xpanxion.com>
2022-04-06 16:04:04 -0400

    update

--- solutions/ex5.js
+++ solutions/ex5.js
@@ -3,22 +3,27 @@ AWS.config.update({region: 'us-east-1'});
 
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+const d = {
+    firstName: "Marty", 
+    lastName: "Burolla"
+}
+
+
 const uploadParams = {
   TableName: 'CUSTOMER_LIST',
   Item: {
-    'CUSTOMER_ID' : {N: '002'},
-    'CUSTOMER_NAME' : {S: 'Marty Burolla'}
+    'CUSTOMER_ID' : {N: '003'},
+    'CUSTOMER_NAME' : {S: JSON.stringify(d)}
   }
 };
 
 const queryParams = {
     TableName: 'CUSTOMER_LIST',
     Key: {
-      'CUSTOMER_ID': {N: '001'}
+      'CUSTOMER_ID': {N: '003'}
     },
     ProjectionExpression: 'CUSTOMER_NAME'
 };
-
 
 const insert = async () => {
     try {
@@ -32,7 +37,8 @@ const insert = async () => {
 
 const getItem = async () => {
     const r = await ddb.getItem(queryParams).promise();
-    console.log(r);
+    const b = JSON.parse(r.Item.CUSTOMER_NAME.S);
+    console.log(b);
 }
 
 insert();
